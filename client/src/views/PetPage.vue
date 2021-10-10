1f634b1d620dc466c4c090d0b60a27161543704f
Alexis JC <alexisjc.dev@gmail.com>
2021-10-10 18:32:54 +0200

    fix: layout pet page

--- client/src/views/PetPage.vue
+++ client/src/views/PetPage.vue
@@ -12,13 +12,13 @@ pet.value = petList.find(pet => pet.id == petId)
 </script>
 
 <template>
-  <div class="m-8 max-w-4xl">
-    <header>
+  <div class="p-8 m-8 max-w-4xl mx-auto">
+    <header class="mb-10">
       <router-link to="/dashboard" class="underline">
         Back to the dashboard
       </router-link>
     </header>
-    <main v-if="pet" class="bg-white rounded shadow-2xl m-8 mx-auto">
+    <main v-if="pet" class="bg-white rounded shadow-2xl">
       <PetCard  :id="pet.id" :name="pet.name" />
     </main>
   </div>
